5a084fd4991a6aebb23c05766bcb3b4e1288373d
Avrahm <avrahmk@me.com>
2020-12-22 07:55:22 -0500

    added react navigation packages

---
+++ src/MainApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
+
+export default function App(){
+
+    <NavigationContainer>
+        {/* {My APP} */}
+        <View/>
+    </NavigationContainer>
+}
