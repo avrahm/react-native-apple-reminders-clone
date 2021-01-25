a60882771dcb76cfac372555e59a51c7a0fe6b50
Avrahm <avrahmk@me.com>
2021-01-24 23:03:47 -0500

    new AddListScreen with color options, icon options, ADD_LIST reducer

---
+++ src/Screens/AddListScreen.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react'
+import { Button, TextInput } from 'react-native'
+import { View, Text } from 'react-native'
+import { Input } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
+import { useDispatch } from 'react-redux';
+import { TouchableOpacity } from 'react-native';
+
+export default function AddListScreen({ navigation }) {
+
+    const dispatch = useDispatch();
+
+    const dispatchAction = (action, payload) => {
+        navigation.navigate('ListScreen')
+        switch (action) {
+            case 'add':
+                dispatch({ type: 'ADD_LIST', payload: payload })
+        }
+    }
+
+    //use state to edit the todo object received from params
+    const [editableList, updateEditableList] = useState({
+        title: '',
+        icon: '',
+        color: ''
+    });
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () =>
+                <Button title='Done' disabled={editableList.title == '' && true} onPress={() => dispatchAction('add', editableList)} />,
+        });
+    }, [editableList])
+
+    const colorOptions = ['red', 'green', 'blue', 'orange', 'gray'];
+
+    const iconOptions = ['barbell', 'baseball-outline', 'airplane', 'basket-outline']
+
+    return (
+        <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 25 }}>
+            <View style={{ height: 100, width: 100, backgroundColor: editableList.color || 'gray', borderRadius: 50, justifyContent: 'center', alignItems: 'center' }}>
+                <View>
+                    {editableList.icon && <Ionicons name={editableList.icon} size={60} />}
+                </View>
+            </View>
+            <View style={{ padding: 20, width: '80%' }}>
+                <Input
+                    placeholder='Title'
+                    value={editableList.title}
+                    onChangeText={(e) => updateEditableList({ ...editableList, title: e })}
+                    style={{ textAlign: 'center', padding: 10 }}
+                />
+            </View>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', padding: 15 }}>
+                {colorOptions.map((eachColor, index) => (
+                    <TouchableOpacity
+                        onPress={() => updateEditableList({ ...editableList, color: eachColor })}
+                        key={index}
+                        style={{ width: 40, height: 40, backgroundColor: eachColor, borderRadius: 50, margin: 5 }}
+                    >
+
+                    </TouchableOpacity>
+                ))}
+                {iconOptions.map((eachIcon, index) => (
+                    <TouchableOpacity
+                        onPress={() => updateEditableList({ ...editableList, icon: eachIcon })}
+                        key={index}
+                        style={{ width: 40, height: 40, backgroundColor: 'gray', borderRadius: 50, margin: 5, justifyContent: 'center', alignItems: 'center' }}
+                    >
+                        <Ionicons name={eachIcon} size={24} color='white' />
+                    </TouchableOpacity>
+                ))}
+            </View>
+        </View>
+    )
+}
--- src/Screens/ListScreen.js
+++ src/Screens/ListScreen.js
@@ -1,30 +1,26 @@
 import React from "react";
 
-import { StyleSheet, Text, ScrollView, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, ScrollView, TouchableOpacity, View, Button } from "react-native";
 import Constants from "expo-constants";
 import { useSelector } from "react-redux";
-import { ListItem, Icon, SearchBar } from 'react-native-elements'
+import { ListItem, Icon, SearchBar } from 'react-native-elements';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import ButtonComponent from '../Components/ButtonComponent';
-import { Button } from "react-native";
 
-export default function ListScreen() {
 
-  const todos = useSelector(state => state.todos.todos)
+export default function ListScreen({ navigation }) {
 
-  const list = [
-    {
-      title: 'Appointments',
-      icon: 'av-timer'
-    },
-    {
-      title: 'Trips',
-      icon: 'flight-takeoff'
-    }
-  ]
+  const todos = useSelector(state => state.todos.todos).filter(todo => !todo.complete);
+  const dueTodayTodos = todos.filter(todo => new Date(todo.dueDate).getDate() == new Date().getDate());
+  const inboxTodos = todos.filter(todo => todo.listId === 0);
+
+  const list = useSelector(state => state.lists.lists)
+
   return (
     <View style={styles.container}>
-      <SearchBar lightTheme={true} placeholder='Search' />
+      {/* <SearchBar lightTheme={true} placeholder='Search' /> */}
       <ScrollView>
         <View style={{ flexDirection: 'column' }}>
           <View style={{ flexDirection: 'row' }}>
@@ -33,17 +29,17 @@ export default function ListScreen() {
                 <ButtonComponent icon='mail-outline' />
                 <Text>Inbox</Text>
               </View>
-              <Text>0</Text>
+              <Text style={{ fontSize: 34 }}>{inboxTodos.length}</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.cardView}>
               <View style={{ alignItems: 'center' }}>
                 <ButtonComponent icon='calendar' />
                 <Text>Today</Text>
               </View>
-              <Text>0</Text>
+              <Text style={{ fontSize: 34 }}>{dueTodayTodos.length}</Text>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity style={styles.cardView}>
+          <TouchableOpacity style={styles.cardView} onPress={() => navigation.navigate('PendingTodoScreen')}>
             <View style={{ alignItems: 'center' }}>
               <ButtonComponent icon='archive-outline' />
               <Text>All</Text>
@@ -60,13 +56,13 @@ export default function ListScreen() {
             My Lists
         </Text>
 
-          <Button title='Add new list'/>
+          <Button title='Add new list' onPress={() => navigation.navigate('AddListScreen')} />
         </View>
         <View>
           {
             list.map((item, i) => (
               <ListItem key={i} bottomDivider>
-                <Icon name={item.icon} />
+                <Ionicons name={item.icon} size={24} color={'white'} style={{ backgroundColor: item.color, borderRadius: 50 }} />
                 <ListItem.Content>
                   <ListItem.Title>{item.title}</ListItem.Title>
                 </ListItem.Content>
