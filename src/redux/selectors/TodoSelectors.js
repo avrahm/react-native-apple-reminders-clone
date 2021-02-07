b18618785c1cf0ffa008ddd5de5442e651ad23a5
Avrahm <avrahmk@me.com>
2021-02-07 11:46:45 -0500

    update project with reselect and selectors

---
+++ src/redux/selectors/TodoSelectors.js
@@ -0,0 +1,80 @@
+
+
+//https://www.digitalocean.com/community/tutorials/redux-reselect
+//https://medium.com/@parkerdan/react-reselect-and-redux-b34017f8194c
+//https://stackoverflow.com/questions/60187910/reselect-redux-with-react-hooks
+//https://react-redux.js.org/next/api/hooks#useselector-examples
+//https://github.com/reduxjs/reselect#accessing-react-props-in-selectors
+//https://github.com/toomuchdesign/re-reselect
+// https://www.youtube.com/watch?v=frT3to2ACCw&feature=share
+//understanding reselect
+
+import { createSelector } from 'reselect';
+
+const getVisibilityFilter = state => state.visibilityFilter
+const getTodos = state => state.todos
+
+export const getVisibleTodos = createSelector(
+    [getVisibilityFilter, getTodos],
+    (visibilityFilter, todos) => {
+        switch (visibilityFilter) {
+            case 'SHOW_ALL':
+                return todos
+            case 'SHOW_COMPLETED':
+                return todos.filter(t => t.complete)
+            case 'SHOW_ACTIVE':
+                return todos.filter(t => !t.complete)
+        }
+    }
+)
+
+export const selectTodosByList = (state, listId) => {
+    if (listId === undefined) return state
+    return state.filter(todo => todo.listId === listId)
+}
+
+export const completeTodos = (state) => {
+    return state.filter(todo => !todo.complete);
+}
+
+export const dueTodayTodos = (state) => {
+    return state.filter(todo => new Date(todo.dueDate).getDate() == new Date().getDate());
+}
+
+export const inboxTodos = (state) => {
+    return state.filter(todo => todo.listId === 0);
+}
+
+
+// const getTodos = useSelector(state => state.getTodos)
+
+// export const selectNumOfDoneTodos = () => createSelector(
+//     (state) => state.todos,
+//     (todos) => todos.filter((todo) => todo.complete)
+// )
+
+
+// export const getTodosByListId = (state, props) => state.filter(todo => todo.listId === props.listId);
+
+// export const makeGetTodosFromListCompleted = () => {
+//     return createSelector(
+//         getTodosByListId,
+//         (todos) => todos.filter(todo => todo.complete)
+//     )
+// }
+
+// export const filterCompleted = () => createSelector(
+//     (state) => state.todos,
+//     (todos) => todos.filter(todo => !todo.complete)
+// )
+
+// export const todosDueToday = createSelector(
+//     (state) => state.todos,
+//     (todos) => todos.filter(todo => new Date(todo.dueDate).getDate() == new Date().getDate())
+// )
+
+
+// export const selectNumOfCompleteTodos = createSelector(
+//     (state) => state.todos,
+//     (todos) => todos.filter((todo) => todo.complete).length
+// )
