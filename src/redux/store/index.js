946a322fc8074fb0001e29c9e22284a52c655d4f
Avrahm <avrahmk@me.com>
2021-02-26 08:34:13 -0500

    installed thunk to handle async dispatches

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,16 @@
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 import TodoListReducer from '../reducers/TodoListReducer';
 
+//combine Reducers 
+const rootReducer = combineReducers({
+    todoLists: TodoListReducer,
+})
+
+//The middleware function thunk allows a redux store to make asynchronous AJAX requests
+//actions muct be plain objects. middleware allows for async actions
 
 //create the store: the applications state
-//combine Reducers 
-const store = createStore(combineReducers({
-    todoLists: TodoListReducer,
-}))
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
