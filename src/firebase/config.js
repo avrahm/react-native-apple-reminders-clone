f7c550d92ddeeffe39d2cb59da88ae2df4191633
Avrahm <avrahmk@me.com>
2021-03-02 20:29:11 -0500

    install firebase from google, initialize firebase in config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+import { firebaseConfig } from './firebaseConfig';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
