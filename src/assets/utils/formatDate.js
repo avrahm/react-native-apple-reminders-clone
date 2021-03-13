6890d8aff6ff8ff3669027ae6f5d174954ef118d
Avrahm <avrahmk@me.com>
2021-03-13 05:43:40 -0500

    new date format with time

--- src/assets/utils/formatDate.js
+++ src/assets/utils/formatDate.js
@@ -3,3 +3,5 @@ import { format } from 'date-fns';
 export const formatDateWithoutDay = date => format(new Date(date), 'LLL d, yyyy').toString();
 
 export const formatDateWithDay = date => format(new Date(date), 'ccc LLL d, yyyy').toString();
+
+export const formatDateWithTime = date => format(new Date(date), 'yyyy-cc-dd hh:mm').toString();
