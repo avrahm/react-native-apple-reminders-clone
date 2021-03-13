b9afa609b1851981f6fad4f96e0d0f2b7e3a971c
Avrahm <avrahmk@me.com>
2021-03-13 07:40:55 -0500

    began creating colors for app theme

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,6 +1,37 @@
-const colors = {
-    bgMain: '',
-    bgSuccess: ''.
-};
+// https://medium.com/eightshapes-llc/light-dark-9f8ea42c9081
 
-export default colors;
+function appTheme(mode) {
+    let colors = {};
+    switch (mode) {
+        case 'dark':
+            colors = {
+                bgPrimary: '#374744',
+                bgSecondary: '#262328',
+                inputBgPrimary: '#ECEFF1',
+                inputFocus: '#FFFFFF',
+                inputText: '#262328',
+                fontMPrimary: '#FFFFFF',
+                fontSecondary: '#ECEFF1',
+                fontDisabled: '',
+                buttonPrimary: '',
+                buttonSecondary: '',
+            };
+            break;
+        default:
+            colors = {
+                bgPrimary: '#ECEFF1',
+                bgSecondary: '#FFFFFF',
+                inputBgPrimary: '#374744',
+                inputFocus: '#262328',
+                inputText: '#FFFFFF',
+                fontMPrimary: '#262328',
+                fontSecondary: '#374744',
+                fontDisabled: '',
+                buttonPrimary: '#ECEFF1',
+                buttonSecondary: '',
+            };
+            break;
+    }
+    return colors;
+}
+export default appTheme;
