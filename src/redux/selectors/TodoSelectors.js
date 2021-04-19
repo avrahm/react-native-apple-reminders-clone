7e9ad3fa85691bef03f86db9469ca681cdb23e2f
Avrahm <avrahmk@me.com>
2021-04-19 00:51:23 -0400

    refactored with eslint

--- src/redux/selectors/TodoSelectors.js
+++ src/redux/selectors/TodoSelectors.js
@@ -8,7 +8,6 @@
 // https://www.npmjs.com/package/reselect
 // understanding reselect
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { formatDateWithDay } from '../../assets/utils/formatDate';
 // import { createSelector } from 'reselect';
 
@@ -20,113 +19,89 @@ export const starterList = {
     showCompletedTasks: false,
 };
 
-export const getAllLists = (state = starterList) => state.map(eaList => eaList.list);
+export const getAllLists = (state = { starterList }) => {
+    if (state === undefined) return [];
+    return state.map(eaList => eaList.list);
+};
 
 export const getList = (state = starterList, listId) => {
-
     if (listId === undefined) return state;
     return state.map(eaList => eaList.list).filter(eaList => eaList.id === listId)[0];
-
 };
 
 export const getListIndex = (state = starterList, listId) => {
-
     if (listId === undefined) return state;
     return state.map(eaList => eaList.list).findIndex(eaListId => eaListId.id === listId);
-
 };
 
 export const getTodosByList = (state = starterList, listId) => {
-
+    if (state === undefined) return 0;
     if (listId === undefined) return state;
     const listIndex = getListIndex(state, listId);
     return state[listIndex].data;
-
 };
 
 export const getAllTodosWithoutList = (state = starterList) => {
-
+    if (state === undefined) return 0;
     const data = [];
     state.map(eaList => {
-
         if (eaList.data.length > 0) {
-
             eaList.data.map(eaTodo => {
-
                 data.push(eaTodo);
-
             });
-
         }
-
     });
     return data;
     // return state;
-
 };
 
 export const getShowCompletedTasksStatusByList = (state = starterList, listId) => {
-
     if (listId === undefined) return state;
     return state[getListIndex(state, listId)].showCompletedTasks;
-
 };
 
 export const getCompleteTodos = (state = starterList) => state.filter(todo => !todo.complete);
 
 export const getDueTodayTodos = (state = starterList) => {
-
+    if (state === undefined) return 0;
     const data = getAllTodosWithoutList(state);
     return data.filter(eaTodo => eaTodo.dueDate === formatDateWithDay(new Date()));
-
 };
 
 export const inboxTodos = (state = starterList) => state.filter(todo => todo.listId === 0);
 
 export const searchTodos = (state = starterList, searchText) => {
-
     if (searchText === undefined) return state;
     return state.filter(eachTodo => (
         eachTodo.title.toLowerCase().includes(searchText.toLowerCase())
     ));
-
 };
 
 export const getSearchTodos = (state = starterList, searchTerm) => {
-
     if (searchTerm === '') return state;
     return state.reduce((result, sectionData) => {
-
         if (!result || !sectionData) return state;
         const { list, data } = sectionData;
         const filteredData = data.filter(element => element.title.toLowerCase().includes(searchTerm.toLowerCase())).filter(element => !element.complete);
         if (filteredData.length !== 0) {
-
             result.push({
                 list,
                 data: filteredData,
             });
-
         }
         return result;
-
     }, []);
-
 };
 
 export const getCompletedTodosFromAllTodos = (state = starterList) => state.reduce((result, sectionData) => {
-
     if (!result || !sectionData) return state;
     const { list, data } = sectionData;
     const completeData = data.filter(element => !element.complete);
     if (completeData.length !== 0) {
-
         result.push({
             list,
             data: completeData,
         });
-
     }
     return result;
-
 }, []);
